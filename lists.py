1378d37224f027d257a4bd81b18fcafa32702dea
gaciuki2402 <delbrivigrace@gmail.com>
2021-12-08 12:20:36 +0300

    My second push....

---
+++ lists.py
@@ -0,0 +1,17 @@
+x=[1,2,[3,4,5],6,7] #declare
+print(x)
+# [1,2,[3,4,5],6,7]
+# 0, 1, 2[0,1,2],3,4
+
+print(x[0])
+print(x[3])
+print(x[4])
+print(x[2])
+print(x[2][0])
+print(x[2][1])
+print(x[2][2])
+#Slicing
+print(x[0:4])
+print(x[0:])
+print(x[:4])
+print(x[2:4])
