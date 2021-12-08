01e838dd5520db1acfbec79d9fbf74553626640d
gaciuki2402 <delbrivigrace@gmail.com>
2021-12-08 14:31:17 +0300

    if statements

---
+++ ifstatements.py
@@ -0,0 +1,19 @@
+a=89
+b=780
+if a<b:
+    print(True)
+else:
+    print(False)
+
+if a>b:
+    print(True)
+else:
+    print(False)
+
+
+
+isRaining=True
+if isRaining:
+    print('wash my clothes')
+else:
+    print("don't wash my clothes")
