c7db3e9f112d521f590f7cdd5d0b4cb2eb875e5a
gaciuki2402 <delbrivigrace@gmail.com>
2021-12-08 12:02:07 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,74 @@
+# integer
+a = 2
+print(a)
+#integer
+b=2345677
+print(b)
+
+#floating point
+pi=3.14
+print(pi)
+
+#string
+c='a'
+print(c)
+
+#string
+name='grace gaciuki'
+print(name)
+
+#boolean
+q=True
+print(q)
+
+#empty value or null data type
+x=None
+print(x)
+
+a = 2
+print(type(a))
+
+b = 34368372389409
+print(type(b))
+
+pi = 3.14
+print(type(pi))
+
+c = 'A'
+print(type(c))
+
+town = 'nairobi'
+print(type(town))
+
+q = True
+print(type(q))
+
+x = None
+print(type(x))
+
+a, b, c =1, 2, 3
+print(a, b, c)
+
+a, b, _ = 1, 2, 3
+print(a, b)
+
+a = b = c = 1
+print(a, b, c)
+
+b = 2
+print(a, b, c)
+
+x = y = [2, 3, 4]
+x =[5, 6, 7]
+print (y)
+
+x = y =[7, 5, 9]
+x[0] = 13
+print(y)
+
+x = [1, 2, [3, 4, 5], 6, 7,]
+print (x[2])
+
+print( x[2][1])
+
+
