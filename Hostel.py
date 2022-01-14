751cba54f08d19a2872583527032f91d0a7930d3
gaciuki2402 <delbrivigrace@gmail.com>
2022-01-14 18:25:50 +0300

    python being so fun

--- Hostel.py
+++ Hostel.py
@@ -3,24 +3,26 @@ class Hostel_Bookings():
         self.ResidentsList=[]
         self.number_of_residents=eval(input("Evaluate number of residents:"))
         for resident in range (self.number_of_residents):
-            if self.number_of_residents>3:
+            if self.number_of_residents>4:
                 print( "No Space Available.")
+                break
+            else:
 
-            username=input("Name:")
-            adm=input("AdmNO:")
-            Year=input("Academic_Year:")
-            hostel=input("Hostel Name:")
-            room=input("RoomNo:")
-            myDict={
-                "Name":username,
-                "AdmNO":adm,
-                "Academin Year":Year,
-                "Hostel Name":hostel,
+                username=input("Name:")
+                adm=input("AdmNO:")
+                Year=input("Academic_Year:")
+                hostel=input("Hostel Name:")
+                room=input("RoomNo:")
+                myDict={
+                    "Name":username,
+                    "AdmNO":adm,
+                    "Academic Year":Year,
+                    "Hostel Name":hostel,
                 "RoomNO":room
             }
             self.ResidentsList.append(myDict)
-            print(self.ResidentsList)
+            print("...Hostel Booked Successfully...")
+        print(self.ResidentsList)
 
 H1=Hostel_Bookings()
 H1.Residents_Details()
-print("...Hostel Booked Successfully...")
