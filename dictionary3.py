1d3fc7db14546ff0ce17152786e8f8af5904e5dd
gaciuki2402 <delbrivigrace@gmail.com>
2022-01-16 08:47:26 +0300

    initial commit

---
+++ dictionary3.py
@@ -0,0 +1,18 @@
+Units={1:"Calculus III",2:"Linear Algebra",3:"Data Base",4:"Vector Analysis",5:"Programming"}
+for key, value in Units.items():
+    print(key, value)
+Foods={1:"Chapo",2:"Githeri",3:"Beans",4:"Pilau",5:"Rice",6:"Pojo",7:"Ugali"}
+print(Foods.get(2))
+print(Foods.get(4))
+print(Foods.popitem())
+print(Foods)
+print(Foods.values())
+print(list(Foods.items()))
+print(len(Foods))
+patient={"Name:":"Princess","Age:":34,"Location:":"Nkabune","Weight:":"90 pounds","Children:":{"grace","kimberly"},"Diagnosis:":["TB","Cholera","Malaria"]}
+print(patient.values())
+for key, value in patient.items():
+    print(key, value)
+print(patient.popitem())
+print(patient.get("Location"))
+print(patient.get("Diagnosis"))
